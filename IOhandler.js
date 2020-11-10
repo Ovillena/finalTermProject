36563ddac29798fb47e6623329aaedf4dde5ff1a
Octavio Villena <octaviovillena@hotmail.com>
2020-11-09 18:54:03 -0800

    finished grayscale funct and main.js. Applied eslint AirBnB styling fixes

--- IOhandler.js
+++ IOhandler.js
@@ -2,10 +2,10 @@
  * Project: COMP1320 Milestone 1
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
- * 
- * Created Date: 
- * Author: Octavio Villena
- * 
+ *
+ * Created Date: Nov 8, 2020
+ * Author: Octavio Villena A01207939
+ *
  */
 
 const { rejects } = require('assert');
@@ -16,112 +16,96 @@ const unzipper = require('unzipper'),
   PNG = require('pngjs').PNG,
   path = require('path');
 
-
 /**
  * Description: decompress file from given pathIn, write to given pathOut 
- *  
- * @param {string} pathIn 
- * @param {string} pathOut 
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
+  const filePathIn = path.normalize(pathIn); // normalize paths for windows
+  const filePathOut = path.normalize(pathOut);
   return new Promise((resolve, reject) => {
-    const fileExtension = pathIn.slice(pathIn.length - 4, pathIn.length); // gets file extension
-    const zipFilePath = path.join(__dirname, pathIn);
-    const outFilePath = path.join(__dirname, pathOut);
-    if (fileExtension != ".zip" || !fs.existsSync(zipFilePath)) { // error if extension is not '.zip' or if file doesnt exist
-      reject("This is the wrong file type or the file does not exist");
+    const fileExt = filePathIn.slice(filePathIn.length - 4, filePathIn.length); // gets file ext
+    if (fileExt !== '.zip' || !fs.existsSync(filePathIn)) { // error if extension is not '.zip' or if file doesnt exist
+      reject('This is the wrong file type or the file does not exist');
     } else {
-      fs.createReadStream(zipFilePath)
-        .pipe(unzipper.Extract({ path: outFilePath }) // unzips the folder
-        )
-      resolve("Unzipping was successful");
+      fs.createReadStream(filePathIn)
+        .pipe(unzipper.Extract({ path: filePathOut })) // unzips the folder to extraction path
+        .on('error', reject)
+        .on('finish', resolve)
+      console.log('Extraction opertaion complete');
     }
-  })
+  });
 };
 
-// unzip("myfile.zip", "unzipped")
-//   .then(message => console.log(message))
-//   .catch(err => console.log(err));
-
 /**
- * Description: read all the png files from given directory and return Promise containing array of each png file path 
- * 
- * @param {string} path 
+ * Description: read all the png files from given directory 
+ * and return Promise containing array of each png file path 
+ *
+ * @param {string} path
  * @return {promise}
  */
-const readDir = dir => {
+const readDir = (dir) => {
+  const thisFilePath = path.normalize(dir);
   return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err, files) => { // reads directory and returns array of file/folder names
+    fs.readdir(thisFilePath, (err, files) => { // reads dir and returns array of file/folder names
       if (err) {
         reject(err);
       } else {
-        let dirArray = [];
-        files.forEach(element => { // loops through files array and creates new array of filepaths to png images
-          if (element.includes(".png")) {
-            dirArray.push(path.join(dir, element)); 
+        const dirArray = [];
+        files.forEach((element) => { // loops through files array, makes new array of png filepaths
+          if (element.includes('.png')) {
+            dirArray.push(path.join(thisFilePath, element));
           }
         });
         resolve(dirArray);
       }
-    })
-  })
+    });
+  });
 };
 
-
-
 /**
- * Description: Read in png file by given pathIn, 
+ * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
- * 
- * @param {string} filePath 
- * @param {string} pathProcessed 
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
  * @return {promise}
  */
 const grayScale = (pathIn, pathOut) => {
-
-  const newPath = path.join(__dirname, "grayscaled"); // creates greyscaled folder if it doesnt exist already
-  if (!fs.existsSync(newPath)) {
-    fs.mkdirSync(newPath);
+  const sourcePath = (pathIn);
+  const thisPathOut = path.normalize(pathOut); // normalize path for windows
+  // creates greyscaled folder if it doesnt exist already
+  if (!fs.existsSync(thisPathOut)) {
+    fs.mkdirSync(thisPathOut);
   }
-
-  const sourcePath = pathIn;
-  const destinationPath = path.join(newPath, pathOut); // string of the new file path
+  const destPath = path.join(thisPathOut, path.basename(sourcePath)); // string of the new file path
   return new Promise((resolve, reject) => {
     if (!fs.existsSync(sourcePath)) { // error if the filepath is invalid
-      reject("Filepath is invalid or file does not exist");
+      reject('Filepath is invalid or file does not exist');
     } else {
       fs.createReadStream(sourcePath) // reads png image data to create array of pixel data
-        .pipe(new PNG())
-        .on("parsed", function () { 
-          for (let y = 0; y < this.height; y++) {// loops through array of pixel data
-            for (let x = 0; x < this.width; x++) {
-              let idx = (this.width * y + x) << 2;
-              let grey= 0.2126*(this.data[idx]) + 0.7152*(this.data[idx + 1]) + 0.0722*(this.data[idx + 2]); // formula to make pixel grey
-              this.data[idx] =  grey; 
-              this.data[idx + 1] = grey;
-              this.data[idx + 2] = grey; //make pixels grey by applying formula each RGB value
-            }
+        .pipe(new PNG({ filterType: 4 }))
+        .on('parsed', function () {
+          for (let i = 0; i < this.data.length; i++) { // loops through array of pixel data
+            const grey = 0.2126 * (this.data[i]) + 0.7152 * (this.data[i + 1]) + 0.0722 * (this.data[i + 2]);
+            // formula to make pixel grey
+            this.data[i] = grey;
+            this.data[i + 1] = grey;
+            this.data[i + 2] = grey; // make pixels grey by applying formula each RGB value
+            i += 3;
           }
-          this.pack().pipe(fs.createWriteStream(destinationPath)); // write data to greyscaled folder
-        })
-      resolve("The image has been greyscaled")
+          this.pack().pipe(fs.createWriteStream(destPath)); // write data to greyscaled folder
+        });
+      resolve('The image has been greyscaled');
     }
-
-  })
+  });
 };
-
-
-
-// readDir(path.join(__dirname, "unzipped"))
-//   .then(result => {
-//     grayScale(result[2], "out.png")
-//   })
-//   .catch(err => console.log(err));
-
 
 module.exports = {
   unzip,
   readDir,
-  grayScale
+  grayScale,
 };
